c01cc22bb789f4182f35a11f0fd3b751c809b3d0
Armand <nobrearmand@gmail.com>
2024-07-14 23:03:11 +0200

    :hammer: removed Intro and merged it to Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
-'use client'; 
-import { useEffect, useState } from 'react';
-import Hero from '../components/Hero/page';
-import Introduction from '../components/Introduction/page';
-import MainContent from '@/components/MainContent/page';
+import Hero from "../components/Hero/page";
+import MainContent from "@/components/MainContent/page";
+import BackToTop from "@/components/BackToTop/page";
 
 export default function Home() {
-    const [data, setData] = useState<any>(null);
-
-    useEffect(() => {
-        fetch('/api/fetch-antelopes')
-            .then((response) => response.json())
-            .then((data) => setData(data))
-            .catch((error) => console.error('Error fetching data:', error));
-    }, []);
-
     return (
-        <section className='flex flex-col justify-center items-center'>
+        <section className="flex flex-col justify-center items-center">
             <Hero />
-            <Introduction />
             <MainContent />
-            {/* {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>} */}
+            <BackToTop />
         </section>
     );
 }
