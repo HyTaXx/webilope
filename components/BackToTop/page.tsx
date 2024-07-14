0b2e98dd698c16743fab7f696d4b6e6d1499e2e7
Armand <nobrearmand@gmail.com>
2024-07-14 23:00:51 +0200

    :hammer: added navigation in navbar and back to top

---
+++ components/BackToTop/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <a
+            href="#"
+            className="fixed bottom-5 right-5 w-8 bg-white rounded-full"
+        >
+            <img src="images/arrow_top.png" alt="" />
+        </a>
+    );
+};
+
+export default page;
--- components/Navbar/page.tsx
+++ components/Navbar/page.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-        <div className="flex items-center justify-between w-full">
-            <div className="flex items-center space-x-4">
-                <Link href="/" className='flex flex-row items-center'>
-                    <img src="images/logo.svg" alt="Webilope Logo" className='w-[40px] h-[40px]' />
-                    <p className="text-lg font-semibold text-primary">Webilope</p>
-                </Link>
+    return (
+        <header className="flex h-20 w-full shrink-0 items-center fixed px-4 md:px-6">
+            <div className="flex items-center justify-between w-full">
+                <div className="flex items-center space-x-4">
+                    <Link href="/" className="flex flex-row items-center">
+                        <img
+                            src="images/logo.svg"
+                            alt="Webilope Logo"
+                            className="w-[40px] h-[40px]"
+                        />
+                        <p className="text-2xl font-semibold text-primary">
+                            Webilope
+                        </p>
+                    </Link>
+                    <Link href="#antelopes" className="text-lg hover:underline">
+                        Antelopes ?
+                    </Link>
+                    <Link href="#infos" className="text-lg hover:underline">
+                        More infos
+                    </Link>
+                    <Link href="#stats" className="text-lg hover:underline">
+                        Stats
+                    </Link>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
     );
-}
+};
 
-export default page
+export default page;
